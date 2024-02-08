fb5ee662dfb9450a3ae5b2803292522e520bef79
SRAKIB17 <rakibulssc5@gmail.com>
2024-02-08 18:48:43 +0600

    'doen

--- Hello.java
+++ Hello.java
@@ -2,6 +2,10 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("I am learning Java.");
+        System.out.println("Hello World!");
         System.out.println("It is awesome!");
+        System.out.println(3);
+        System.out.println(358);
+        System.out.println(50000);
     }
 }
